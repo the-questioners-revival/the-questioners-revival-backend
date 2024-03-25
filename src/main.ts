993ce8101cf71f2b882b15d874fd421b7d763a6a
Joseph Maloch <jozefmaloch@gmail.com>
2024-03-25 22:58:48 +0800

    Make it listen on port 4001.

--- src/main.ts
+++ src/main.ts
@@ -32,6 +32,6 @@ async function bootstrap() {
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, document);
 
-  await app.listen(4000);
+  await app.listen(4001);
 }
 bootstrap();
