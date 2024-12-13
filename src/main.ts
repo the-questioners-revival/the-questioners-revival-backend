5666b269e9dea8a235a6904552c4a1b0d1593e00
Joseph Maloch <jozefmaloch@gmail.com>
2024-12-13 23:09:32 +0800

    Update CORS and helmet in main.ts to allow for cross-origin-resource-policy.

--- src/main.ts
+++ src/main.ts
@@ -10,15 +10,27 @@ async function bootstrap() {
   dotenv.config();
   const app = await NestFactory.create(AppModule);
 
-  app.use(helmet()).enableCors({
-    origin: [
-      'http://localhost:3001',
-      'https://the-questioners-revival-frontend.vercel.app',
-      process.env.ORIGIN
-    ],
-  });
+  app.use(
+    helmet({
+      crossOriginResourcePolicy: false, // Disable Cross-Origin-Resource-Policy
+      contentSecurityPolicy: {
+        useDefaults: true,
+        directives: {
+          'img-src': ["'self'", 'data:', 'blob:', 'http://localhost:4001'], // Allow images from localhost:4001
+        },
+      },
+    })
+  );
 
+  // Enable CORS
+  app.enableCors({
+    origin: ['http://localhost:3001', 'https://the-questioners-revival-frontend.vercel.app', process.env.ORIGIN],
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+    allowedHeaders: 'Content-Type, Authorization',
+    credentials: true, // Allow cookies
+  });
   app.use(cookieParser());
+
 
   // Swagger setup
   const config = new DocumentBuilder()
